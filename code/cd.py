2a8e5e919cfa093429d6c2e5751d1bae32f8398a
davidwhogg <david.hogg@nyu.edu>
2016-03-03 15:52:46 -0500

    mixture of gaussians class

--- code/cd.py
+++ code/cd.py
@@ -1,10 +1,16 @@
 """
 This file is part of the CombinatoricDegeneracies project.
 Copyright 2016 David W. Hogg & Dan Foreman-Mackey.
+
+# to-do list:
+- Make multiply function that can multiply the likelihood function by a prior.
+- Figure out how to visualize things.
+- Write sampling code.
 """
 
 import itertools as it
 import numpy as np
+import pylab as plt
 
 def factorial(N):
     if N == 0:
@@ -14,9 +20,45 @@ def factorial(N):
 
 def choose(N, M):
     """
-    bad implementation
+    ## bugs:
+    - bad implementation (will fail at large inputs)
     """
     return factorial(N) / factorial(M) / factorial(N-M)
+
+class mixture_of_gaussians:
+    """
+    ## bugs:
+    - May not work at `K==1` or `D==1`.
+    """
+
+    def __init__(self, amps, means, ivars):
+        self.K = len(amps)
+        assert amps.shape == (self.K,)
+        assert np.all(amps > 0.)
+        self.amps = amps
+        KK, D = means.shape
+        assert KK == self.K
+        self.D = D
+        self.means = means
+        assert ivars.shape == (self.K, self.D, self.D)
+        for ivar in ivars:
+            assert np.all(ivar.T == ivar)
+        self.ivars = ivars
+        self.logdets = np.zeros(self.K)
+        for ivar in ivars:
+            slogdet = np.linalg.slogdet(ivar)
+            print(slogdet)
+
+    def value(self, x):
+        assert x.shape == self.means[0].shape
+        val = 0.
+        for k in range(self.K):
+            delta = x - self.means[k]
+            val += self.amps[k] * np.exp(-0.5 * np.dot(delta, np.dot(self.ivars[k], delta)) + 0.5 * self.logdets[k])
+        return val
+
+    def __call__(self, x):
+        return np.log(self.value(x))
 
 def get_log_likelihood(M, K, D, ivar_scale=256., ndof=None):
     """
@@ -63,12 +105,16 @@ def get_log_likelihood(M, K, D, ivar_scale=256., ndof=None):
             bigamps[i] *= amps[p[k]]
             bigmeans[i, k * D : k * D + D] = means[p[k]]
             bigivars[i, k * D : k * D + D, k * D : k * D + D] = ivars[p[k]]
-    print(bigamps)
+    bigamps /= McKKfac
 
-    def ln_like(pars):
-        # return mixture_of_gaussians(bigamps, bigmeans, bigivars)
-        return 0.
-    return ln_like
+    return mixture_of_gaussians(bigamps, bigmeans, bigivars)
 
 if __name__ == "__main__":
     ln_like = get_log_likelihood(7, 5, 3)
+    plt.clf()
+    xis = np.arange(-2., 2., 0.01)
+    xs = np.zeros((len(xis), 5 * 3))
+    xs[:,0] = xis
+    ln_Ls = np.array([ln_like(x) for x in xs])
+    plt.plot(xis, np.exp(ln_Ls - np.max(ln_Ls)), "k-")
+    plt.savefig("cd.png")
